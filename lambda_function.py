705c4c1e56445f461a821e24887121a6c8a0a98e
beshlawy <ubix@integrant.com>
2024-02-28 14:54:26 +0200

    Update lambda code

--- lambda_function.py
+++ lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Beshlawy')
+        'body': json.dumps('Hello from Mohamed El-Beshlawy who is our DevOps Engineer')
     }
